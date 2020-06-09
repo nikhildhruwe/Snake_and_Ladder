e5324569abf8d567d77ea7cc72521510921c48cd
unknown <nick.dhruwe@gmail.com>
2020-06-09 19:50:41 +0530

    below zero condition

--- Snake_Ladder.sh
+++ Snake_Ladder.sh
@@ -4,8 +4,23 @@ echo "       Welcome To Snake and Ladder Game."
 echo "Single Player"
 
 pos=0
-echo "player at start position: $pos"
+echo -e "player at start position: $pos \n"
 
+function ladder(){
+
+	pos=$(($pos+$diceRoll))
+
+}
+
+function snake(){
+	pos=$(($pos-$diceRoll))
+
+	if (( $pos<0 ))
+	then
+		echo "restarting from postion: 0"
+		pos=0
+	fi
+}
 
 diceRoll=$(( RANDOM%6 +1 ))
 echo "dice rolled: $diceRoll"
@@ -14,11 +29,11 @@ check=$((RANDOM%3))
 case $check in
 					0) echo "No play: remain in same place"
 					;;
-					1)	pos=$(($pos+$diceRoll))
-					 	echo "Ladder:move ahead"
+					1)	echo "Ladder : move ahead by $diceRoll"
+						ladder $pos $diceRoll
 					;;
-					2)	pos=$(($pos-$diceRoll))
-						echo "Snake:move behind"
+					2)	echo "Snake  : move behind by $diceRoll"
+						snake $pos $diceRoll
 					;;
 esac
 echo "Position :$pos"
