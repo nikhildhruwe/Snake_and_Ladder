4c2895f60fb6148aa2630713efd30f18808bd23b
unknown <nick.dhruwe@gmail.com>
2020-06-09 19:08:46 +0530

    added 3 cases for each turn

--- Snake_Ladder.sh
+++ Snake_Ladder.sh
@@ -2,10 +2,23 @@
 
 echo "       Welcome To Snake and Ladder Game."
 echo "Single Player"
-pos=0
 
+pos=0
 echo "player at start position: $pos"
 
-diceRoll=$(( RANDOM%6 +1 ))
 
+diceRoll=$(( RANDOM%6 +1 ))
 echo "dice rolled: $diceRoll"
+
+check=$((RANDOM%3))
+case $check in
+					0) echo "No play: remain in same place"
+					;;
+					1)	pos=$(($pos+$diceRoll))
+					 	echo "Ladder:move ahead"
+					;;
+					2)	pos=$(($pos-$diceRoll))
+						echo "Snake:move behind"
+					;;
+esac
+echo "Position :$pos"
