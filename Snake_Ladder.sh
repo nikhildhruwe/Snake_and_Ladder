78ef4ef9a2e617aa82e9d24e9c61e73b4a7d7b3d
unknown <nick.dhruwe@gmail.com>
2020-06-10 11:51:48 +0530

    Added 2 player simulation

--- Snake_Ladder.sh
+++ Snake_Ladder.sh
@@ -3,30 +3,30 @@
 echo -e "        Welcome To Snake and Ladder Game.\n"
 echo "Two Players"
 
-pos1=0
-pos2=0
+position1=0
+position2=0
 dicePlayed2=0
 dicePlayed1=0
 echo -e "Player1 and Player2  at starting position 0\n"
 
 function ladder(){
-   pos=$(($pos+$diceRoll))
-   if (($pos>100))
+   position=$(($position+$diceRoll))
+   if (($position>100))
    then
       echo "player got above 100,remain in same place"
-      pos=$(($pos-$diceRoll))
+      position=$(($position-$diceRoll))
    fi
-   echo "   Position:$pos"
+   echo "   Position:$position"
 }
 
 function snake(){
-   pos=$(($pos-$diceRoll))
-   if (( $pos<0 ))
+   position=$(($position-$diceRoll))
+   if (( $position<0 ))
    then
       echo "   restarting from postion: 0"
-      pos=0
+      position=0
    fi
-   echo "   Position:$pos"
+   echo "   Position:$position"
 }
 
 function diceRoll(){
@@ -36,38 +36,38 @@ function diceRoll(){
 
 function game(){
          diceRoll=$1
-         pos=$2
+         position=$2
   check=$((RANDOM%3))
    case $check in
                0) echo "   option, No play: remain in same place"
-                  echo "   Position:$pos"
+                  echo "   Position:$position"
                ;;
-               1) if (( $(($pos+$diceRoll))<100))
+               1) if (( $(($position+$diceRoll))<100))
                   then
                      echo "   option, Ladder : move ahead by $diceRoll and play again"
                   fi
-                  ladder $pos $diceRoll
-						if (($pos!=100))
+                  ladder $position $diceRoll
+						if (($position!=100))
                   then
                      diceRoll=$((RANDOM%6+1))
               			((dicePlayed++))
 					      echo "   Dice rolled :$diceRoll"
-                     game $diceRoll $pos
+                     game $diceRoll $position
                   else
-                     echo "   Reached position $pos"
+                     echo "   Reached position $position"
                   fi
 
                ;;
                2) echo "   option, Snake  : move behind by $diceRoll"
-                  snake $pos $diceRoll
+                  snake $position $diceRoll
                ;;
    esac
- return $pos
+ return $position
 }
 
 function winCheck(){
-	pos=$1
-	if (( $pos==100 ))
+	position=$1
+	if (( $position==100 ))
 	then
        echo 1
 	else
@@ -82,10 +82,10 @@ do
    diceRoll1=$( diceRoll )
    ((dicePlayed1++))
    echo -e "\nPlayer 1 , dice rolled:$diceRoll1"
-   game $diceRoll1 $pos1
-   pos1=$?
+   game $diceRoll1 $position1
+   position1=$?
 	dicePlayed1=$(($dicePlayed1+$dicePlayed))
-	check=$(winCheck $pos1)
+	check=$(winCheck $position1)
 	if (($check==1))
 	then
 		break;
@@ -94,10 +94,10 @@ do
    diceRoll2=$( diceRoll )
    ((dicePlayed2++))
    echo -e "\nPlayer 2 , dice rolled:$diceRoll2"
-   game $diceRoll2 $pos2
-   pos2=$?
+   game $diceRoll2 $position2
+   position2=$?
 	dicePlayed2=$(($dicePlayed2+$dicePlayed))
-	check=$(winCheck $pos2)
+	check=$(winCheck $position2)
    if (($check==1))
    then
       break;
@@ -106,7 +106,7 @@ done
 echo -e "\nNumber of times Player 1 played: $dicePlayed1"
 echo -e "Number of times Player 2 played: $dicePlayed2\n"
 
-if (($pos1==100))
+if (($position1==100))
 then
    echo -e "\nPlayer 1 has Won the game"
 else
